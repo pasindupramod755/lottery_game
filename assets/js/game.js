6613d4cbcd4a00d4b89ed2b3eda7d0bca0521d5d
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-12 21:29:18 +0530

    Random load validated

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,41 @@
-document.getElementById("inputField").innerHTML = `
-    <input type="text" id="field" placeholder="A">
-        <input type="text" id="field0" placeholder="1">
-        <input type="text" id="field1" placeholder="1">
-        <input type="text" id="field2" placeholder="1">
-        <input type="text" id="field3" placeholder="1">
-        <input type="button" value="OK">
+
+document.getElementById("youInput").innerHTML = `
+    <input type="text" id="inputfield" placeholder="A">
+        <input type="text" id="inputfield0" placeholder="1">
+        <input type="text" id="inputfield1" placeholder="1">
+        <input type="text" id="inputfield2" placeholder="1">
+        <input type="text" id="inputfield3" placeholder="1">
+        <input type="button" value="OK" onclick="loadRandomNumber()" id="okBtn">
 `
 
 
-for (let i = 0; i < 4; i++) {
-    let randInt = Math.floor(Math.random() * 80) + 1;
-    console.log(randInt);
-    document.getElementById("field"+i).value = randInt;
+function loadRandomNumber() {
+    document.getElementById("okBtn").disabled = true;
+    document.getElementById("inputField").innerHTML = `
+        <h2 style="text-align: center;">Win Number</h2>
+        <input type="text" id="field" disabled>
+        <input type="text" id="field0" disabled>
+        <input type="text" id="field1" disabled>
+        <input type="text" id="field2" disabled>
+        <input type="text" id="field3" disabled>
+        <input type="button" value="↻" onclick="location.reload()">
+`
+    randomNumber();
 }
 
-const englishLetterKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let englishLetterIndex = Math.floor(Math.random() * 26);
-let englishLetter = englishLetterKey.charAt(englishLetterIndex);
-document.getElementById("field").value = englishLetter;
-console.log(englishLetter);
+function randomNumber() {
 
+    for (let i = 0; i < 4; i++) {
+        let randInt = Math.floor(Math.random() * 80) + 1;
+        console.log(randInt);
+        document.getElementById("field" + i).value = randInt;
+    }
+
+    const englishLetterKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let englishLetterIndex = Math.floor(Math.random() * 26);
+    let englishLetter = englishLetterKey.charAt(englishLetterIndex);
+    document.getElementById("field").value = englishLetter;
+    console.log(englishLetter);
+}
