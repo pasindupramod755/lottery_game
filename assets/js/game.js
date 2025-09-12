7ecc3bf3d0a958460f7ab219b0c1a08359cff52e
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-12 21:03:56 +0530

    Random Loaded

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,23 @@
 document.getElementById("inputField").innerHTML = `
-    <input type="text" id="field1" placeholder="A">
+    <input type="text" id="field" placeholder="A">
+        <input type="text" id="field0" placeholder="1">
+        <input type="text" id="field1" placeholder="1">
         <input type="text" id="field2" placeholder="1">
         <input type="text" id="field3" placeholder="1">
-        <input type="text" id="field4" placeholder="1">
-        <input type="text" id="field5" placeholder="1">
         <input type="button" value="OK">
 `
 
 
+for (let i = 0; i < 4; i++) {
+    let randInt = Math.floor(Math.random() * 80) + 1;
+    console.log(randInt);
+    document.getElementById("field"+i).value = randInt;
+}
+
+const englishLetterKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let englishLetterIndex = Math.floor(Math.random() * 26);
+let englishLetter = englishLetterKey.charAt(englishLetterIndex);
+document.getElementById("field").value = englishLetter;
+console.log(englishLetter);
 
