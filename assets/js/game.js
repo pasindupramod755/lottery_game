54b587c381cc45147dc47ac1f163c13badfa856d
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-12 20:19:28 +0530

    Add Main

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+document.getElementById("inputField").innerHTML = `
+    <input type="text" id="field1" placeholder="A">
+        <input type="text" id="field2" placeholder="1">
+        <input type="text" id="field3" placeholder="1">
+        <input type="text" id="field4" placeholder="1">
+        <input type="text" id="field5" placeholder="1">
+        <input type="button" value="OK">
+`
